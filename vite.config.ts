dc7381246caff313699ad5bd3639e2342f2e44d3
João Victor <109232731+Jsilva703@users.noreply.github.com>
2025-10-06 21:13:17 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
     },
     plugins: [react()],
-    base: "/JV-PERFORMANCE-DIGITAL/",
+    base: '/',
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
